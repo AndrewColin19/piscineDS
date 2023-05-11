485128db94fd9e6c1603d5e6cc111c4669e4f14e
andrew <andrew19@live.fr>
2023-05-11 10:46:11 +0200

    modif ex00 pds01

--- pds01/ex02/remove_duplicates.py
+++ pds01/ex02/remove_duplicates.py
@@ -8,7 +8,7 @@ conn.autocommit = True
 cursor = conn.cursor()
 
 sql = '''CREATE TABLE temp (LIKE customers);
-        INSERT INTO temp SELECT DISTINCT * FROM customers;
+        INSERT INTO temp SELECT DISTINCT ON (event_type, product_id, price, user_id, user_session)* FROM customers;
         DROP TABLE customers;
         ALTER TABLE temp RENAME TO customers;'''
 
